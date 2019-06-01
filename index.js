36b9535313692dd7b1dfcfe6aa7981239b2e2645
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 20:28:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,9 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  const ranks = document.getElementById('main').querySelectorAll('ul.ranked-list');
+  const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  
+  for (let i = 0; i < ranks.length; i++){
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + 1;
+  }
 }

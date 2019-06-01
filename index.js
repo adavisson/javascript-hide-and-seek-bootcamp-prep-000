6d46799c17c8b6825a0be20c6f0da325792d4a3a
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 20:40:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function deepestChild(){
   var element = document.getElementById('grand-node');
   
   function iterate(element){
-    if(element.children){
+    if(element.querySelector('div')){
       iterate(element.querySelector('div'));
     }else
     return element;

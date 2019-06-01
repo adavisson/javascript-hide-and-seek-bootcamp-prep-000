79e2d66138c8d080f690d03589c369301421a236
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 20:19:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function getFirstSelector(selector){
 function nestedTarget(){
   //var nest = document.getElementById('nested');
   //var tar = nest.children;
-  return document.getElementById('nested').querySelectorAll('div div div div.target');
+  return document.getElementById('nested').getElementsByClassName('target');
 }

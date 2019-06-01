f460294164e7a339e7b504905c6575deb1247461
Andrew Davisson <akdavisson4@gmail.com>
2019-06-01 20:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementByClassName(document.getElementByID());
+  return document.getElementByClassName(document.getElementById());
 }
